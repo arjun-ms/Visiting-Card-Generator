c7131ee04fdc459f761104c036df332601c921ed
arjun-ms <arjunachu123.aa@gmail.com>
2024-01-20 18:35:27 +0530

    Visiting Card Generator | Completed namaste-react-tutorial #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
 
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-
+  const [userData,setUserData] = useState({
+    name:"",
+    phno:"",
+    email:"",
+    website:"",
+    submit:false
+  })
   return (
     <>
       <div className="header">
-        Namaste React
+        Visiting Card Generator 
+      </div>
+
+      <div className="details">
+        <input onChange={(e)=>setUserData({...userData,name:e.target.value})} type="text" placeholder='Name' /><br />
+        <input onChange={(e)=>setUserData({...userData,phno:e.target.value})} type="text" placeholder='Phone Number'/><br />
+        <input onChange={(e)=>setUserData({...userData,email:e.target.value})} type="text" placeholder='Email' /><br />
+        <input onChange={(e)=>setUserData({...userData,website:e.target.value})} type="text" placeholder='Website'/><br />
+        <input type="submit" value="Generate" onClick={()=>setUserData({...userData,submit:true})}/><br />
+      </div>
+
+      <div>
+          {
+            userData.name &&
+            userData.phno &&
+            userData.email &&
+            userData.website &&
+            userData.submit && (
+              <>
+                <div className="card">
+                  <p>
+                    <b>Name: &nbsp;</b> {userData.name}
+                  </p>
+                  <p>
+                    <b>Phno: &nbsp;</b> {userData.phno}
+                  </p>
+                  <p>
+                    <b>Email: &nbsp;</b> {userData.email}
+                  </p>
+                  <p>
+                    <b>Website: &nbsp;</b> {userData.website}
+                  </p>
+                </div>
+              </>
+            )
+          }
       </div>
     </>
   )
